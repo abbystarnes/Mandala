bff9a5b1db0732c8eec13bfc0515465258079c4e
Abby Starnes <abbymstarnes@gmail.com>
2017-09-13 17:40:36 -0500

    svg loading

---
+++ public/js/app.service.js
@@ -0,0 +1,18 @@
+
+(function() {
+  'use strict'
+
+  angular.module('myApp')
+    .service('appService', service)
+
+  service.$inject = ['$http']
+  function service($http) {
+    const vm = this;
+
+    // get all svgs
+    // get current svg url
+    // read file from current svg url
+
+  }
+
+}());
--- public/js/mandala/mandala.component.js
+++ public/js/mandala/mandala.component.js
@@ -7,44 +7,27 @@
       templateUrl: 'js/mandala/mandala.template.js'
     })
 
-  controller.$inject = ['$http']
-  function controller($http) {
+  controller.$inject = ['$http', 'appService']
+  function controller($http, appService) {
     const vm = this
 
     vm.$onInit = onInit
-    // vm.addExpense = addExpense
 
     function onInit() {
-      // $http.get('/api/expenses').then(function (response) {
-      //   vm.expenses = response.data
-      // })
-      vm.expenses = 'chocolate candies'
-      vm.svg = document.getElementById('svg');
-      console.log(vm.svg);
 
-      vm.gs = document.getElementsByClassName('st0');
-      console.log(vm.gs);
-
-
-      for (let x = 0; x < vm.gs.length; x++){
-        vm.gs[x].addEventListener("click", vm.changeColor);
-      }
+    $http.get('img/mandalas/mandala1.svg').
+     then(function onSuccess(response) {
+        console.log(response.data);
+     }).
+     catch(function onError(response) {
+      console.log(response);
+     });
     }
 
     vm.changeColor = function(){
       vm.current_color = document.getElementById('colorpicker').value;
-      console.log(vm.current_color, 'hey color', this);
       this.style.fill = vm.current_color;
     }
-
-    // function addExpense() {
-    //   $http.post('/api/expenses', vm.expense)
-    //     .then(function (response) {
-    //       vm.expenses.push(response.data)
-    //       delete vm.expense
-    //     })
-    // }
-
   }
 
 }());
