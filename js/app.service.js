771fbd658654bb7f1dc44d89b7a0993c0e01ef03
Abby Starnes <abbymstarnes@gmail.com>
2017-09-18 16:47:03 -0500

    seperated out server-side

--- js/app.service.js
+++ js/app.service.js
@@ -8,7 +8,16 @@
   service.$inject = ['$http']
   function service($http) {
     const vm = this;
+    const app = 'https://mandala-capstone-server.herokuapp.com';
 
+    vm.getUsers = function() {
+      $http.get(`${app}/users`).then(function (response){
+        console.log(response);
+      })
+      return;
+    }
+
+    vm.getUsers();
     // get all svgs
     // get current svg url
     // read file from current svg url
