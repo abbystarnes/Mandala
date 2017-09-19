42c3688011f3e37833c7f2ef66e490a29b99e200
Abby Starnes <abbymstarnes@gmail.com>
2017-09-19 16:40:33 -0500

    user can select a mandala with loaded fill colors

--- js/mandala/mandala.component.js
+++ js/mandala/mandala.component.js
@@ -14,67 +14,35 @@
     vm.templates = appService.templates;
     vm.fills = appService.fills;
     vm.template_thumbnails = [];
+    vm.active_svg;
     vm.$onInit = onInit
     function onInit() {
-
       appService.getFills.then(function(data){
-        // console.log(vm.fills);
         appService.getTemplates.then( function(){
           for (let x = 0; x < vm.templates.length; x++){
-            // console.log(vm.templates[x], x);
-            vm.createTemplateThumbnails(vm.templates[x].file_path, x, vm.templates[x].id, data);
+            vm.createTemplateThumbnails(vm.templates[x].file_path, x, vm.templates[x].id, data, 'thumbnail');
           }
         });
       });
-
-
-      // promise.then((templates) =>{
-      //   console.log(templates[2], 'here');
-        // for (let x = 0; x < templates.length; x++){
-        //   console.log('calling');
-        //   console.log(templates[x], x);
-        //   vm.createTemplateThumbnails(templates[x], x);
-        // }
-
-
-      // console.log(vm.templates);
-
-
-      // $http.get('img/templates/2.svg').
-      //  then(function onSuccess(response) {
-      //     vm.element = response.data;
-      //     vm.currentpath;
-      //     vm.mem_fill;
-      //     vm.svg = document.getElementById('svg');
-      //     vm.svg.innerHTML = response.data;
-      //     vm.paths = document.getElementsByClassName("st0");
-      //     for (let x = 0; x < vm.paths.length; x++){
-      //       vm.paths[x].addEventListener("click", vm.changeColor);
-      //     }
-      //  }).
-      //  catch(function onError(response) {
-      //  });
     }
 
-    // vm.createTemplateThumbnails(response.data[x].file_path);
-    //
+    vm.selectMandala = function(template_id, template_file_path){
+      console.log(vm.fills[0].color_array);
+      vm.createTemplateThumbnails(template_file_path, 0, template_id, vm.fills, 'active_svg');
+    }
 
-    // ([,])#
-    //
-    // ([,])r
-    // /([,])(?=r)/
-    // /([,])(?=#)/
-
-    vm.createTemplateThumbnails = function(template_url, index, template_id, fills){
+    vm.createTemplateThumbnails = function(template_url, index, template_id, fills, svg_class_name){
       $http.get(template_url).
       then(function onSuccess(response){
-
-        vm.empty_svgs = document.getElementsByClassName('thumbnail');
+        vm.empty_svgs = document.getElementsByClassName(svg_class_name);
         vm.empty_svgs[index].innerHTML = response.data;
         for (let y = 0; y < fills.length; y++){
           if (fills[y].template_id === template_id) {
             let paths = vm.empty_svgs[index].getElementsByClassName('st0');
-            fills[y].color_array = fills[y].color_array.split(/([,])(?=#)|([,])(?=r)/);
+            // console.log(fills[y].color_array);
+            if(!(Array.isArray(fills[y].color_array))){
+              fills[y].color_array = fills[y].color_array.split(/([,])(?=#)|([,])(?=r)/);
+            }
 
             let counter = 0;
             vm.dedup = function(array){
@@ -97,7 +65,6 @@
             for (let x = 0; x < paths.length; x++){
               paths[x].style.fill = fills[y].color_array[x];
             }
-            console.log(paths[0].style.fill, fills[y].color_array[0]);
           }
         }
 
--- js/mandala/mandala.template.js
+++ js/mandala/mandala.template.js
@@ -1,6 +1,6 @@
 <div class="svg-container">
 <?xml version="1.0" encoding="utf-8"?>
-<svg version="1.1" id="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+<svg version="1.1" id="active_svg" class="active_svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 424 644" style="enable-background:new 0 0 424 644;" xml:space="preserve">
 </svg>
 </div>
@@ -9,9 +9,10 @@
 <div id="slide-out" class="side-nav">
 	<ul>
 		<li ng-repeat="template in $ctrl.templates">
-		<svg version="1.1" class="thumbnail" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+		<a class="mandala-link" ng-click="$ctrl.selectMandala(template.id, template.file_path)"><svg version="1.1" class="thumbnail" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 			 viewBox="0 0 424 644" style="enable-background:new 0 0 424 644;" xml:space="preserve">
-		</svg></li>
+		</svg></a>
+		</li>
 	</ul>
 </div>
 <a href="#" class="button-collapse" data-activates="slide-out" data-sidenav="left" data-menuwidth="500" data-closeonclick="false">
