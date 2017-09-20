053199c2b2b8df57ab19e2d9f90ab11102c211d1
Abby Starnes <abbymstarnes@gmail.com>
2017-09-20 10:32:54 -0500

    change fill working on dynamically selected svg

--- js/mandala/mandala.component.js
+++ js/mandala/mandala.component.js
@@ -10,6 +10,7 @@
   controller.$inject = ['$http', 'appService']
   function controller($http, appService) {
     const vm = this
+    vm.current_fill;
     vm.elements = [];
     vm.templates = appService.templates;
     vm.fills = appService.fills;
@@ -27,8 +28,8 @@
     }
 
     vm.selectMandala = function(template_id, template_file_path){
-      console.log(vm.fills[0].color_array);
       vm.createTemplateThumbnails(template_file_path, 0, template_id, vm.fills, 'active_svg');
+
     }
 
     vm.createTemplateThumbnails = function(template_url, index, template_id, fills, svg_class_name){
@@ -39,7 +40,6 @@
         for (let y = 0; y < fills.length; y++){
           if (fills[y].template_id === template_id) {
             let paths = vm.empty_svgs[index].getElementsByClassName('st0');
-            // console.log(fills[y].color_array);
             if(!(Array.isArray(fills[y].color_array))){
               fills[y].color_array = fills[y].color_array.split(/([,])(?=#)|([,])(?=r)/);
             }
@@ -60,38 +60,64 @@
                 return vm.dedup(array);
               }
             }
-
+            vm.current_fill = fills[y];
             vm.dedup(fills[y].color_array);
             for (let x = 0; x < paths.length; x++){
               paths[x].style.fill = fills[y].color_array[x];
             }
+            if (svg_class_name === 'active_svg'){
+              for (let x = 0; x < paths.length; x++){
+                paths[x].addEventListener("click", vm.changeColor);
+              }
+            }
           }
         }
-
-        // vm.templates.push(response.data);
       })
     }
 
+    // vm.update_fill = function(fill_id) {
+      // get new fill properties
+      // send to db
+    // }
 
 
-    vm.undo = function(){
-      vm.currentpath.style.fill = vm.mem_fill;
-    }
 
-    vm.save = function(){
-      vm.colors = [];
-      for (let x = 0; x < vm.paths.length; x++){
-        if (vm.paths[x].style.fill){
-          vm.colors.push(vm.paths[x].style.fill);
-        } else {
-          vm.colors.push('#fff');
-        }
-      }
-      console.log(vm.colors);
-      console.log(vm.colors.join(','));
-    }
+    // vm.undo = function(){
+    //   vm.currentpath.style.fill = vm.mem_fill;
+    // }
+    //
+    // vm.save = function(){
+    //   vm.colors = [];
+    //   for (let x = 0; x < vm.paths.length; x++){
+    //     if (vm.paths[x].style.fill){
+    //       vm.colors.push(vm.paths[x].style.fill);
+    //     } else {
+    //       vm.colors.push('#fff');
+    //     }
+    //   }
+    //   console.log(vm.colors);
+    //   console.log(vm.colors.join(','));
+    // }
+    //let gs = document.getElementsByClassName('st0');
+    // console.log(gs);
+    //
+    // let changeColor = function(){
+    //   let current_color = document.getElementById('color').value;
+    //   console.log(current_color, 'hey color', this);
+    //   this.style.fill = current_color;
+    // }
+    //
+    // for (let x = 0; x < gs.length; x++){
+    //   gs[x].addEventListener("click", changeColor);
+    // }
 
     vm.changeColor = function(){
+      console.log('clicking in change');
+
+      let current_color = document.getElementById('colorpicker').value;
+      console.log(current_color, 'hey color', this);
+      this.style.fill = current_color;
+
       vm.currentpath = this;
       if (this.style.fill){
         vm.mem_fill = this.style.fill;
